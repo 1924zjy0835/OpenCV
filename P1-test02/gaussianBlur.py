b4a59a41cbb15db9f796409e46050301f3dfba11
1924zjy0835 <1924083517@qq.com>
2020-04-23 10:13:07 +0800

    高斯图像加上噪声点

--- P1-test02/gaussianBlur.py
+++ P1-test02/gaussianBlur.py
@@ -18,7 +18,7 @@ def clamp(value):
         return value
 
 
-def GaussianBlur(image):
+def Gaussian_noise(image):
     h, w, c = image.shape
     # 遍历每一个像素点，为其加上一个随机数的值，就可以得到高斯值
     for row in range(h):
@@ -33,19 +33,23 @@ def GaussianBlur(image):
             image[row, col, 1] = clamp(b + clamp(random_value[1]))
             image[row, col, 2] = clamp(b + clamp(random_value[2]))
 
-    cv.imshow("noise image", image)
+    cv.imshow("Gaussian image", image)
 
 
 src = cv.imread("./images/flowers01.jpg")
-cv.imshow("Gaussian image", src)
+cv.imshow("原图", src)
 
 # 统计时间
 # getTickCount()函数返回操作系统启动到当前所经过的计时周期数
 t1 = cv.getTickCount()
-GaussianBlur(src)
+Gaussian_noise(src)
 t2 = cv.getTickCount()
 # getTickFrequency()函数，返回CPU的频率
 time = (t2 - t1)/cv.getTickFrequency()
 print("耗费时间：%s"%time)  # 耗费时间：10.2369628
+
+# GaussianBlur(src, ksize, sigmaX, dst=None, sigmaY=None, borderType=None)
+dst = cv.GaussianBlur(src, (0, 0), 15)
+cv.imshow("Gaussian image 2", dst)
 cv.waitKey(0)
 cv.destroyAllWindows()
